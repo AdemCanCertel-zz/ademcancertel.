bdb94c1ccdaeabbf3ed105dc66ae8e1a34ff428f
AdemCan <69676558+AdemCanCertel@users.noreply.github.com>
2021-07-29 18:07:35 +0300

    🚀 New tippy.js tooltip framework
    New tippy.js tooltip

---
+++ assets/js/tippy.js
@@ -0,0 +1,28 @@
+    // E-mail
+    tippy('#email', {
+        content: 'Mail me',
+        arrow: false,
+        animation: 'scale',
+      });
+    // Steam
+      tippy('#steam', {
+          content: 'View my Steam profile',
+          arrow: false,
+          animation: 'scale',
+      })
+    // Github
+      tippy('#github', {
+          content: 'View my GitHub profile',
+          arrow: false,
+          animation: 'scale'
+      })
+    // Twitter
+      tippy('#twitter', {
+          content: 'View my Twitter profile',
+          animation: 'scale'
+      })
+    // Twitter 
+      tippy('#discord', {
+          content: 'Join my Discord server',
+          animation: 'scale',
+      })
