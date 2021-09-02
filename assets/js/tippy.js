1307e820b1240e1367270637a8e8d9d993b1b7be
AdemCan <69676558+AdemCanCertel@users.noreply.github.com>
2021-09-02 23:53:42 +0300

    New tippyjs emoji
    New tippyjs emoji

--- assets/js/tippy.js
+++ assets/js/tippy.js
@@ -1,49 +1,71 @@
-    // E-mail
-    tippy('#email', {
+  // SOCİAL
+
+  // Mail
+    tippy('#mail',{
         content: 'Mail me',
         arrow: false,
         animation: 'scale',
       });
-    /*
-     Steam
-      tippy('#steam', {
-          content: 'View my Steam profile',
-          arrow: false,
-          animation: 'scale',
-      })*/
-    // Github
-      tippy('#github', {
-          content: 'View my GitHub profile',
-          arrow: false,
-          animation: 'scale'
-      })
-    // Twitter
-      tippy('#twitter', {
-          content: 'View my Twitter profile',
-          animation: 'scale'
-      })
-    /*
-      tippy('#discord', {
-          content: 'Join my Discord server',
-          animation: 'scale',
-      })*/
-    /*
-      tippy('#trello', {
-        content: `Trello plan's`,
-        animation: 'scale'
-      })*/
-    // İnstagram
-      tippy('#instagram', {
-        content: 'View my İntagram profile',
-        animation: 'scale'
-      })
-    // Blog
-      tippy('#blog', {
-        content: 'Blog',
-        animation: 'scale'
-      })
     // Linkedin
-        tippy('#linkedin', {
-            content: 'Linkedin',
-            animation: 'scale'
-        })
+    tippy('#linkedin',{
+      content: 'Linkedin profile',
+      arrow: false,
+      animation: 'scale'
+    })
+    // Github
+    tippy('#github',{
+      content: 'GitHub profile',
+      arrow: false,
+      animation: 'scale'
+    })
+    // İnstagram
+    tippy('#ınstagram',{
+      content: 'Instagram profile',
+      arrow: false,
+      animation: 'scale'
+    })
+    // Twitter
+    tippy('#twitter',{
+      content: 'Follow me on Twitter!',
+      arrow: false,
+      animation: 'scale'
+    })
+
+    // TECH
+
+    // JavaScript
+    tippy('#javascript',{
+      content: 'JavaScript ❤️',
+      arrow: false,
+      animation: 'scale'
+    })
+    // Html5
+    tippy('#html5',{
+      content: 'Html5 ❤️',
+      arrow: false,
+      animation: 'scale'
+    })
+    // NuxtJS
+    tippy('#nuxtjs',{
+      content: 'NuxtJS ❤️',
+      arrow: false,
+      animation: 'scale'
+    })
+    // VueJS
+    tippy('#vuejs',{
+      content: 'VueJS ❤️',
+      arrow: false,
+      animation: 'scale'
+    })
+    // ReactJS
+    tippy('#reactjs',{
+      content: 'ReactJS ❤️',
+      arrow: false,
+      animation: 'scale'
+    })
+    // Figma UI Design
+    tippy('#figma',{
+      content: 'Figma ❤️',
+      arrow: false,
+      animation: 'scale'
+    })
