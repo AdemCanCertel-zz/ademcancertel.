7cd3bbd3aafba0c425459ee208541d71da861474
Eleven <11@apptime.tech>
2021-09-03 11:45:26 +0300

    fix social tippys

--- assets/js/tippy.js
+++ assets/js/tippy.js
@@ -5,30 +5,35 @@
         content: 'Mail me',
         arrow: false,
         animation: 'scale',
+        placement: 'right',
       });
     // Linkedin
     tippy('#linkedin',{
       content: 'Linkedin profile',
       arrow: false,
-      animation: 'scale'
+      animation: 'scale',
+      placement: 'right',
     })
     // Github
     tippy('#github',{
       content: 'GitHub profile',
       arrow: false,
-      animation: 'scale'
+      animation: 'scale',
+      placement: 'right',
     })
     // İnstagram
     tippy('#ınstagram',{
       content: 'Instagram profile',
       arrow: false,
-      animation: 'scale'
+      animation: 'scale',
+      placement: 'right',
     })
     // Twitter
     tippy('#twitter',{
       content: 'Follow me on Twitter!',
       arrow: false,
-      animation: 'scale'
+      animation: 'scale',
+      placement: 'right',
     })
 
     // TECH
