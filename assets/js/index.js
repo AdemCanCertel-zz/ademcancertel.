3075dd29b8ad1d981c081e1965662be35981ec85
AdemCan <69676558+AdemCanCertel@users.noreply.github.com>
2021-09-09 20:52:41 +0300

    new vue.js 👌
    new vue.js 👌

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,7 @@
-console.log("Hello")
+var dab = new Vue({
+    el: '#dab',
+    data: {
+        description_about: `Hi, I am AdemCan Certel 17 years old high school student in Turkey from 2017 I am doing the site with web technologies. 
+        I'm mostly focused on JavaScript, doing web projects through JavaScript. I'm learning TypeScript now.`
+    }
+})
